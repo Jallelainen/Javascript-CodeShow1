632f6f985250cc4cf8308db65257141ec0d1c77e
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-10-26 11:55:30 +0100

    did some stuff

--- myScript.js
+++ myScript.js
@@ -1,5 +1,26 @@
 console.log("from console.log");
 //alert("dont you unless you must?")
-var name = "<h1>Lasse</h1>";
-document.getElementById("demo").innerHTML = name;//rewrote the actual htmlfile
-document.write("this is <b>.write</b>");
+//var name = "<h1>Lasse</h1>";
+
+var myDemoElement = document.getElementById("demo");
+
+//document.getElementById("demo").innerHTML = name;//rewrote the actual htmlfile
+
+//myDemoElement.innerHTML = name;
+//myDemoElement.style.color = "blue"
+
+document.getElementById("changeMe").classList.add("maxW");//added class to id
+
+//document.write("this is <b>.write</b>");
+
+
+function createDivInsideDemo (addClassCss)
+{
+    var myBlock = document.createElement("div");
+    myBlock.classList.add(addClassCss);
+    
+    myDemoElement.appendChild(myBlock);
+}
+
+createDivInsideDemo("chocblock")
+createDivInsideDemo("greenblock")
