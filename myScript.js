7d9c0f178d491b8d3b1c491f8cdf0738d94c53ff
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-10-26 11:21:18 +0100

    first commit

---
+++ myScript.js
@@ -0,0 +1,5 @@
+console.log("from console.log");
+//alert("dont you unless you must?")
+var name = "<h1>Lasse</h1>";
+document.getElementById("demo").innerHTML = name;//rewrote the actual htmlfile
+document.write("this is <b>.write</b>");
